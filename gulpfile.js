002c16e7617353668fb1595ed68ff9a4a0fb136c
Ivan <soloveii@mydigicode.com>
2021-10-18 15:26:18 +0300

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const sass = require('gulp-sass')(require('sass'));
+const clean = require('gulp-clean');
 
 let project_folder = "dist";
 let source_folder = "src";
@@ -15,23 +16,46 @@ const path = {
   }
 };
 
-let { src, dest } = require('gulp'),
-  gulp = require('gulp'),
-  browsersync = require("browser-sync").create();
+function browserSync(params) {
+  browsersync.init({
+    server:{
+      baseDir: project_folder
+    },
+    port: 3000,
+    notify: false
+  })
+};
 
 function html() {
   return src(path.src.html)
     .pipe(dest(path.build.html))
-    .pipe(browsersync.stream())
-}
+    .pipe(browsersync.reload({
+      stream:true
+    }))
+};
 
 function scss() {
   return src(path.src.css)
     .pipe(sass())
     .pipe(dest(path.build.css))
-}
+};
+
+function cleaner() {
+  return src('dist',
+    { read: false })
+    .pipe(clean());
+};
+
+let { src, dest } = require('gulp'),
+  gulp = require('gulp'),
+  browsersync = require("browser-sync").create();
 
 let build = gulp.series(html, scss);
-exports.html = html
-exports.scss = scss
-exports.build = build
+let watch = gulp.parallel(build, browserSync);
+
+exports.html = html;
+exports.build = build;
+exports.watch = watch;
+exports.default = watch;
+exports.clean = cleaner;
+exports.scss = scss;
